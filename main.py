cb2bc9942064180b0bb5b978b1f76f78466a6363
crafzdog <240414100@fellow.lpkia.ac.id>
2024-10-13 10:09:10 +0700

    provides several function docs

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-# entry point for the program
-
-
+# a main() function acts as entry point executed when the script is starts
 def main():
     name = prompt("Insert your name : ")
     print(name)
 
 
+# a function that help us to ask a question to the end user
 def prompt(question: str):
     answer = input(question)
     return answer
 
 
+# ensures that the main() function is executed only when the script is run directly
 if __name__ == "__main__":
     main()
