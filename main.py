a543b907cb3c5673f8e2490fbc2fa39bc9bbc969
crafzdog <240414100@fellow.lpkia.ac.id>
2024-10-14 17:45:26 +0700

    add menu list

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
+from menu import Menu
+
+
 # a main() function acts as entry point executed when the script is starts
 def main():
-    name = prompt("Insert your name : ")
-    print(name)
+    while True:
 
+        print("================ MENU ================")
+        print("1. Tambah Data Mahasiswa.")
+        print("2. Daftar Data Mahasiswa.")
+        print("3. Exit.")
 
-# a function that help us to ask a question to the end user
-def prompt(question: str) -> str:
-    answer = input(question)
-    return answer
+        choice = input("\nMasukkan Menu yang anda pilih : ")
+
+        match (choice):
+            case Menu.TAMBAH.value:
+                print("Tambah Siswa...")
+
+            case Menu.DAFTAR.value:
+                print("Daftar Mahasiswa...")
+
+            case Menu.EXIT.value:
+                print("Exit...")
+                break
 
 
 # ensures that the main() function is executed only when the script is run directly
