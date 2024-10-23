d8fc102c8ecb4851d0164a71d4358da9f8ead2ed
crafzdog <240414100@fellow.lpkia.ac.id>
2024-10-23 11:34:09 +0700

    init file

--- menu.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-
-# menu enum for submission
-class Menu(Enum):
-    Register = ("1", "register")
-    Login = ("2", "login")
-    EXIT = ("0", "exit")
--- user_option.py
+++
@@ -1,8 +0,0 @@
-def user_option():
-    print("1.Data Mahasiswa IF")
-    print("2.Matkul Prody IF")
-    print("3.Exit")
-
-    test = input("WIP(Work in Project)")
-    print("Cya~~~")
-    
