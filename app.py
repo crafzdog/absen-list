3faa83f63a6ae9ff4e107d95e55e9cc7e6514f97
crafzdog <240414100@fellow.lpkia.ac.id>
2024-10-23 13:52:02 +0700

    add /home route

--- app.py
+++ app.py
@@ -2,9 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/home")
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
