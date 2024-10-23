70add4ca7acbfdc61c8d266af35a579abd5cce1e
Najwan Ali <Malingpeak3@gmail.com>
2024-10-23 16:45:44 +0700

    test

--- app.py
+++ app.py
@@ -3,10 +3,14 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/home")
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/test")
+def testing():
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
