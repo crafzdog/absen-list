b951ff82a2ea21e8174adcdcc20381236d0bb716
crafzdog <240414100@fellow.lpkia.ac.id>
2024-10-23 21:31:28 +0700

    add some new pages

--- app.py
+++ app.py
@@ -4,8 +4,23 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/features")
+def features():
+    return render_template("features.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 
 @app.route("/test")
