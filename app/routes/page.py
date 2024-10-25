3bf759848804e95177a23e019485d750cfd36232
crafzdog <240414100@fellow.lpkia.ac.id>
2024-10-25 10:27:24 +0700

    refactor routes

--- app/routes/page.py
+++ app/routes/page.py
@@ -5,9 +5,19 @@ page = Blueprint("page", __name__)
 
 @page.route("/")
 def home_page():
-    return render_template("index.html")
+    return render_template("index.jinja2")
 
 
 @page.route("/features")
 def features_page():
-    return "Features page"
+    return render_template("features.jinja2")
+
+
+@page.route("/about")
+def about_page():
+    return render_template("about.jinja2")
+
+
+@page.route("/contact")
+def contact_page():
+    return render_template("contact.jinja2")
