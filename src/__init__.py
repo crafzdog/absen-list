6e681298153c0baf960a9120ce6a7441537dbac3
crafzdog <240414100@fellow.lpkia.ac.id>
2024-11-02 10:05:44 +0700

    adding .env file for project configuration

--- src/__init__.py
+++ src/__init__.py
@@ -11,11 +11,7 @@ db = SQLAlchemy(model_class=Base)
 def create_app():
     app = Flask(__name__)
 
-    db_path = os.path.join(
-        os.path.abspath(os.path.dirname(__file__)), os.path.relpath("db/database.db")
-    )
-
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ["DB__ENGINE"]
 
     db.init_app(app)
 
